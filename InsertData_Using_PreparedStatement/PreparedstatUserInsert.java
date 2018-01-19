d96e530556ba9e6aa140543b1653571d5eba7660
Nilesh Chaklasiya <33598871+Nilesh2411@users.noreply.github.com>
2018-01-18 20:14:11 -0500

    Update PreparedstatUserInsert.java

--- InsertData_Using_PreparedStatement/PreparedstatUserInsert.java
+++ InsertData_Using_PreparedStatement/PreparedstatUserInsert.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.io.*; 
 public class PreparedstatUserInsert {
 	
-	 
+	 // Oracle Driver. 
 	 
 	public static void main(String args[])throws Exception{  
 	Class.forName("oracle.jdbc.driver.OracleDriver");  
